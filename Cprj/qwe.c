31d4479a469e38425e41dafe5d783a628d4b263f
JieJie136 <156673498@qq.com>
2025-03-10 21:15:53 +0800

    修改 qwe.c，添加用户输入功能

--- Cprj/qwe.c
+++ Cprj/qwe.c
@@ -4,6 +4,6 @@ int main() {
     char name[50];
     printf("Enter your name: ");
     scanf("%s", name);
-    printf("Hello, %s!\n", name);
+    printf("Hello, %s! Welcome to dev branch.\n", name);
     return 0;
 }
